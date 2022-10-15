076f34af1acfba4c98b67fde25a61da5f45046e9
martin-creator <martinlubowa@outlook.com>
2022-10-15 11:27:24 +0300

    Deleted multiplesms php script

--- controllers/multiplesms.php
+++
@@ -1,9 +0,0 @@
-<?php
- include_once './models/sms.php';
-
-    $sms = new Sms('+3444444');
-    $recipients =  $sms->fetchRecipients();
-    $response = $sms->sendSMS("We have slashed our money sending fees by 50%", $recipients);
-    echo json_encode($response);
-    
-?>
